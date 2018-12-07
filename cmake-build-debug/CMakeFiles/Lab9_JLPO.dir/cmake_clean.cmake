cd494f0ee16a4409418018891a29b9637ea7dfb9
jlpereira8 <jlpereira355@gmail.com>
2018-12-07 13:25:51 -0600

    primer p

---
+++ cmake-build-debug/CMakeFiles/Lab9_JLPO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab9_JLPO.dir/main.cpp.o"
+  "Lab9_JLPO.pdb"
+  "Lab9_JLPO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab9_JLPO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
