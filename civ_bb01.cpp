71f4c0bfc207703e7a32a479b38a50631df91825
jlpereira8 <jlpereira355@gmail.com>
2018-12-07 14:45:06 -0600

    Push 3

--- civ_bb01.cpp
+++ civ_bb01.cpp
@@ -3,11 +3,79 @@
 //
 
 #include "civ_bb01.h"
+#include "iostream"
+#include "cstring"
+
+using std::cout;
+using std::endl;
+
+string invertir(int* ,int );
 
 string civ_bb01::decodificar(string texto) {
+
+    //Tokenizer
+
+    char str[texto.size()];
+    for (int i = 0; i < texto.size(); ++i) {
+        str[i]=texto.at(i);
+    }
+    char* point;
+    point = strtok(str,",");
+    str[texto.size()]=NULL;
+    while (point!=NULL){
+        
+        point = strtok(NULL,",");
+    }
+
+    //
+
+
+
     return "";
 }
 
 string civ_bb01::codificar(string texto) {
-    return "";
+    int* a=new int[texto.size()];
+    string* testo=new string[texto.size()];
+    for (int i = 0; i < texto.size(); ++i) {
+        int conv=texto.at(i);
+        a[i]=conv;
+    }
+    for (int j = 0; j < texto.size() ; ++j) {
+        int* bin=new int[8];
+        int num=a[j];
+        int aux =0;
+        while(num>0){
+            int mod=num%2;
+            int nuevo=num/2;
+            bin[aux]=mod;
+            num=nuevo;
+            aux++;
+        }
+
+        string ret=invertir(bin,8);
+        testo[j]=ret;
+
+    }
+    string ret="";
+    for (int k = 0; k < texto.size(); ++k) {
+
+        if (k==texto.size()-1){
+            ret+=testo[k];
+        }else {
+            ret += testo[k] + "-";
+        }
+    }
+
+
+    return ret;
+}
+string invertir(int* x,int s){
+    string o="";
+    int aux=7;
+    for(int i=0;i<s;i++){
+        o=x[aux-i];
+    }
+    return o;
+
 }
