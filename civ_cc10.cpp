4fbe833d54b25ca1d5a90b1c781b241e3e18cd66
Rodrigo Velasquez <rodrix183@hotmail.com>
2018-12-07 14:54:29 -0600

    Clase cc10

--- civ_cc10.cpp
+++ civ_cc10.cpp
@@ -5,11 +5,39 @@
 #include "civ_cc10.h"
 
 string civ_cc10::decodificar(string texto) {
-
-    return "";
+  //cesar
+  string result;
+  int resta = 17;
+  int conver= 0;
+  for(int i = 0; i < texto.size(),i++){
+  //pasado a entero
+  conver = texto[i];
+  if(conver < 97){
+    conver = 97 - conver;
+    conver = 122 - conver;
+  }
+  conver -= resta;
+  //pasar a string
+  result += conver;
+  }
+  return result;
 }
 
 string civ_cc10::codificar(string texto) {
-
-    return "";
+  //cesar
+string result;
+int suma = 17;
+int conver= 0;
+for(int i = 0; i < texto.size(),i++){
+  //pasado a entero
+  conver = texto[i];
+  if(conver > 122){
+    conver = conver - 122;
+    conver = 97 + suma;
+  }
+  conver += suma;
+  //pasar a string
+  result += conver;
+}
+return result;
 }
